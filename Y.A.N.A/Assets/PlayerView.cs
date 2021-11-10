8d02e16242f8141b4eaae174d89e743c9fcd1e98
Twaice <55257468+KronbergerAndreas@users.noreply.github.com>
2021-11-10 20:13:57 +0100

    initial commit

---
+++ Y.A.N.A/Assets/PlayerView.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5df247dd1b8ffc645bac8cc26143d8cc07117053f7c2852765bde1de8b36caf1
+size 852
---
+++ Y.A.N.A/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2b4315fc4a11ab829682fce754d77c6f7e899e5f7284781366893b7a71e9365c
+size 1972
