334db1976210dcc9bdcec8f68a470dcd4eaeae0e
Twaice <55257468+KronbergerAndreas@users.noreply.github.com>
2021-11-16 15:49:40 +0100

    161121 Bugfixes, trys
    Playermovement finished, Lightning adjusted, hitboxes improved

--- Y.A.N.A/Assets/Scripts/PlayerMovement.cs
+++ Y.A.N.A/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36c63a83ee61e30fb6b969c1c518408287960636c67b50526eb88f7f78f0cea3
-size 2999
+oid sha256:0e2bbf7ab3144b23ae86009d4b81e98da33f28cf038990948b09e7cdd0b19ad7
+size 2817
